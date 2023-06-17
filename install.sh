ab7481d8526060c6bd6e49e9e27f5aa553095478
madson7 <madson.adm7@gmail.com>
2023-06-16 23:08:21 -0300

    🎉 install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+while getopts v: option
+do
+case "${option}"
+in
+v) TAG=${TAG};;
+esac
+done
+
+git submodule add -f https://github.com/madson7/docker-environment.git
+git submodule foreach --recursive git checkout $TAG
+git submodule foreach --recursive git pull
+
+cd docker-environment
+cat .env.example > ../.env.example
+cat .env.example > ../.env
+cat readme.md >> ../README.md
+
+make
